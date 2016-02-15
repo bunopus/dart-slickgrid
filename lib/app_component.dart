c92f0da916b4954c219b94b63e2a6149677aa5dd
Evgeny Gusev <eugene.gusev@team.wrike.com>
2016-02-16 00:49:14 +0300

    Added slick interop

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,6 +2,23 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular2/angular2.dart';
+import 'package:dart_slickgrid/slickgrid-wrapper/slickgrid-wrapper.dart' as slick;
 
-@Component(selector: 'my-app', templateUrl: 'app_component.html')
-class AppComponent {}
+@Component(selector: 'my-app', templateUrl: 'app_component.html', encapsulation: ViewEncapsulation.None)
+class AppComponent implements OnInit {
+  AppComponent() {
+
+  }
+
+  void ngOnInit() {
+    var columns = [
+      new slick.Column(id: "title", name: "Title", field: "title")
+    ];
+
+    var items = [{"title":"Test1"}, {"title":"Test2"}, {"title":"Test3"}];
+
+    var options = new slick.Options(enableColumnReorder:false);
+
+    var grid = new slick.Grid("#myGrid", items, columns, options);
+  }
+}
